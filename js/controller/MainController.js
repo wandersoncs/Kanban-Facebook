55c24cba3bbc0765a2eaf53b4748a73e381ba1ff
wandersoncs <wandersoncorreiasouza@gmail.com>
2016-11-23 22:48:51 -0300

    Implementação Controller Principal

--- js/controller/MainController.js
+++ js/controller/MainController.js
@@ -1,5 +1,47 @@
 angular.module('Kanban')
 
-  .controller('MainController', function ($scope) {
+  .controller('MainController', function ($scope, Facebook) {
+
+    $scope.usuario = {};
+    $scope.logado = false;
+
+    var usuarioConectado = false;
+
+    Facebook.getLoginStatus(function (response) {
+      if (response.status == 'connected')
+        userIsConnected = true;
+    });
+
+    $scope.logar = function () {
+      if (!usuarioConectado) {
+        login();
+      }
+    };
+
+    login = function () {
+      Facebook.login(function (response) {
+        if (response.status == 'connected') {
+          $scope.logado = true;
+          me(); //meu perfil
+        }
+      });
+    };
+
+    me = function () {
+      Facebook.api('/me', function (response) {
+        $scope.$apply(function () {
+          $scope.usuario = response;
+        });
+      });
+    };
+
+    $scope.logout = function () {
+      Facebook.logout(function () {
+        $scope.$apply(function () {
+          $scope.usuario  = {};
+          $scope.logado   = false;
+        });
+      });
+    };
 
   });
