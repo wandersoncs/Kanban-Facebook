ca98c58769925821cfc7e31d6b33e13e387b654c
wandersoncs <wandersoncorreiasouza@gmail.com>
2016-12-13 22:34:04 -0300

    remover conteudo do index.html

--- js/controller/GrupoController.js
+++ js/controller/GrupoController.js
@@ -2,96 +2,29 @@ app
 
   .controller('GrupoController', function ($state, Facebook, $rootScope, $scope) {
 
-    $scope.logout = function () {
-      Facebook.logout(function () {
+    var carregarPerfil = function () {
+      Facebook.api('/me', function (response) {
         $scope.$apply(function () {
-          $scope.usuario = {};
-          $rootScope.logado = false;
-          $state.go('login');
+          $scope.usuario = response;
+          getGroups();
         });
       });
     };
 
-    $scope.criarTarefa = function (mensagem) {
-      var url = '/' + $scope.grupo.id + '/feed';
-      mensagem = mensagem + ' #todo';
-      Facebook.api(url, 'POST', {message: mensagem}, function (response) {
-        getFeedGrupo();
+    var getGroups = function () {
+      Facebook.api('/me/groups', function (response) {
+        $scope.$apply(function () {
+          $scope.usuario.grupos = response.data;
+        });
       });
     };
 
-    $scope.alterarTarefa = function (tarefa, tipo) {
-      var url = '/' + tarefa.id;
-      var mensagem = tarefa.message + ' ' + tipo;
-      Facebook.api(url, 'POST', {message: mensagem}, function (response) {
-        if (response && !response.error) {
-          getFeedGrupo();
+    $scope.selecionarGrupo = function (id) {
+      for (grupo in $scope.usuario.grupos) {
+        if (grupo.id === id) {
+          $scope.grupo = grupo;
         }
-      });
+      }
     };
-
-    $scope.deletarTarefa = function (tarefa) {
-      var url = '/' + tarefa.id;
-      Facebook.api(url, 'DELETE', function (response) {
-        if (response && !response.error) {
-          getFeedGrupo();
-        }
-      });
-    };
-
-    $scope.getFeedGrupo = function () {
-      var url = '/' + $scope.grupo.id + '/feed';
-      Facebook.api(url, function (response) {
-        feed = response;
-        getTarefas();
-      });
-    }
-
-    var getTarefas = function () {
-
-      var tarefas = {};
-      var todo = {};
-      var doing = {};
-      var done = {};
-
-      tarefas = feed.data;
-      tarefas = tarefas.filter(function (elem) {
-        if ('message' in elem) {
-          return elem;
-        }
-      });
-
-      todo = tarefas.filter(function (elem) {
-        var mensagem = elem.message;
-        if (mensagem.indexOf('#todo') !== -1) {
-          var msg = mensagem.replace('#todo', '');
-          elem.message = msg;
-          return elem;
-        }
-      });
-
-      doing = tarefas.filter(function (elem) {
-        var mensagem = elem.message;
-        if (mensagem.indexOf('#doing') !== -1) {
-          var msg = mensagem.replace('#doing', '');
-          elem.message = msg;
-          return elem;
-        }
-      });
-
-      done = tarefas.filter(function (elem) {
-        var mensagem = elem.message;
-        if (mensagem.indexOf('#done') !== -1) {
-          var msg = mensagem.replace('#done', '');
-          elem.message = msg;
-          return elem;
-        }
-      });
-
-      $scope.todo = todo;
-      $scope.doing = doing;
-      $scope.done = done;
-    };
-
   })
 ;
